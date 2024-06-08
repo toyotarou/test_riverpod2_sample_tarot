75812211ccdd8e07df29755b7d01f8ef71ce0840
Toyoda Hideyuki <hide.toyoda@gmail.com>
2024-06-08 16:56:49 +0900

    commit 20240608

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [Rest APIs with Riverpod 2 flutter](https://www.youtube.com/watch?v=9JJacpEeOtw)
 ---
+- 17:35 write
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,24 +9,29 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('Tarot List')),
-      body: ref.watch(tarotAllProvider).when(
-            data: (data) {
-              return Card(
-                child: ListView(
-                  children: data
-                      .map(
-                        (e) => ListTile(
-                          title: Text(e.name),
-                          subtitle: Text(e.image),
-                        ),
-                      )
-                      .toList(),
-                ),
-              );
-            },
-            error: (error, stackTrace) => Center(child: Text('$error')),
-            loading: () => const Center(child: CircularProgressIndicator()),
-          ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          ref.refresh(tarotAllProvider);
+        },
+        child: ref.watch(tarotAllProvider).when(
+              data: (data) {
+                return Card(
+                  child: ListView(
+                    children: data
+                        .map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            subtitle: Text(e.image),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                );
+              },
+              error: (error, stackTrace) => Center(child: Text('$error')),
+              loading: () => const Center(child: CircularProgressIndicator()),
+            ),
+      ),
     );
   }
 }
